5b8ecbb12dd3afcf23022947ae98c58d75c3d368
Seanterry <idienumahsokombie@gmail.com>
2026-09-23 23:21:01 +0100

    feat: update Socket.IO CORS configuration to allow all origins

--- client/server/server.js
+++ client/server/server.js
@@ -23,11 +23,10 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
-
 /* =========================
    MIDDLEWARE
 ========================= */
